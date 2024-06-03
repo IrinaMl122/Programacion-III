65344956f62653ea57352bd6ed0c43e633df663d
Mateo Bruno <mbrunobarberon@escuelasproa.edu.ar>
2024-06-03 10:59:47 -0300

    Create Recordatorio.py

---
+++ Trabajo en clases/Recordatorio/Recordatorio.py
@@ -0,0 +1,5 @@
+#Recordatorio
+objetivo= "Ser un gran programador"
+dificultad= "Dificil o incluso imposible"
+recordar_que= f"No importa que tan {dificultad} sea, no te pierdas la vista de tu objetivo de {objetivo}"
+print(recordar_que)
