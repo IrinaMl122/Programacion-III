be9ba6757dcf26444ea5791d9788e3e9beda9784
Mateo Bruno <mbrunobarberon@escuelasproa.edu.ar>
2024-06-05 16:19:23 -0300

    Update Zumbador Activo.ino

--- SolucionesPracticas/Robotica/Zumbador Activo.ino
+++ SolucionesPracticas/Robotica/Zumbador Activo.ino
@@ -1,1 +1,24 @@
+int buzzPin = 8;
+int number;
+String msgl = "Por favor INGRESE su numero";
+int dt = 500;
 
+void setup()
+{
+  Serial.begin (9600);
+  pinMode (buzzPin, OUTPUT);
+}
+
+void loop()
+{
+  Serial.println();
+  Serial.println(msgl);
+  while (Serial.available() == 0) {
+  }
+  number = Serial.parseInt();
+  if (number > 10) {
+    digitalWrite (buzzPin, HIGH);
+    delay (dt);
+    digitalWrite (buzzPin, LOW);
+}
+}
